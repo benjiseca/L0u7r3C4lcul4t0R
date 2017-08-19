6f18e716ab8989f00d099d8fba2c41faae6c0634
cdutartr <cdutartr@student.42.fr>
2017-08-19 22:48:22 +0200

    ft_header modified with ft_is_operand

--- ft_header.h
+++ ft_header.h
@@ -6,7 +6,7 @@
 /*   By: mgaspa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 15:01:33 by mgaspa            #+#    #+#             */
-/*   Updated: 2017/08/19 21:36:15 by mgaspa           ###   ########.fr       */
+/*   Updated: 2017/08/19 22:47:29 by cdutartr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void	ft_putnbr(int nb);
 int		ft_atoi(char *str);
 int		eval_expr(char *str);
 char	*ft_tout_colle(char *str);
+int		ft_is_operand(char c);
 #endif
