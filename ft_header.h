ea7b5c19619af7dd1fc386ea9e2d98007522e6d4
mgaspa <maris.gaspa@gmail.com>
2017-08-19 21:36:51 +0200

    modif header pour ft_tout_colle

--- ft_header.h
+++ ft_header.h
@@ -6,7 +6,7 @@
 /*   By: mgaspa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 15:01:33 by mgaspa            #+#    #+#             */
-/*   Updated: 2017/08/19 18:48:06 by mgaspa           ###   ########.fr       */
+/*   Updated: 2017/08/19 21:36:15 by mgaspa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	ft_putchar(char c);
 void	ft_putnbr(int nb);
 int		ft_atoi(char *str);
 int		eval_expr(char *str);
-char	*ft_tout_coller(char *str);
+char	*ft_tout_colle(char *str);
 #endif
