b68a259134004d29976fb553b0f0c1cb925afa19
mgaspa <maris.gaspa@gmail.com>
2017-08-20 19:38:17 +0200

    Maris ersion

--- ft_header.h
+++ ft_header.h
@@ -6,7 +6,7 @@
 /*   By: mgaspa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 15:01:33 by mgaspa            #+#    #+#             */
-/*   Updated: 2017/08/19 22:52:15 by cdutartr         ###   ########.fr       */
+/*   Updated: 2017/08/20 19:04:42 by mgaspa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@
 
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
+void	ft_putstr(char *str);
 int		ft_atoi(char *str);
 int		eval_expr(char *str);
+int		ft_strlen(char *str);
 char	*ft_tout_colle(char *str);
+float		ft_operations(char sign, float nb1,float nb2);
+float		ft_calc_polonais(char **str);
+char	**ft_split_operand(char *str);
 int		ft_is_operand(char c);
 int		ft_isnot_operand(char c);
+char	**ft_transpolonais(char **str);
 #endif
